901b384f9f4722cd7b377eeb74d56c39c9fc15f8
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-12-08 21:18:35 +0800

    Add hardhat files

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x02AE939CF29484390233D43D88D4D786DA19190C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://crypto-devs-nft-pied.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
